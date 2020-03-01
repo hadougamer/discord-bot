f83ce1694170541ca845da8cb2b3d4b9e9728f7a
Alexandre Conrado <conrado@nasc.io>
2020-03-01 17:20:16 +0100

    Implements a chatbot with wit.ai and discord

---
+++ credentials.dist.js
@@ -0,0 +1,13 @@
+/*
+    1 - Create a copy of this file
+    2 - Rename the file to cretendials.local.js
+    3 - Fill the fields with the real data
+*/
+var cretendials = {
+    // Discord Bot Token
+    BOT_TOKEN : 'PASTE_YOUR_BOT_TOKEN_HERE',
+    // Wit.AI Token
+    WIT_TOKEN : 'PASTE_YOUR_WIT_TOKEN_HERE'
+};
+
+module.exports = cretendials
--- main.js
+++ main.js
@@ -1,16 +1,65 @@
-const Discord = require('discord.js');
+const {Wit, log} = require('node-wit');
+const fs 		 = require('fs');
+const Discord 	 = require('discord.js');
+const bot  		 = new Discord.Client();
 
-const bot = new Discord.Client();
+const credentials = require('./credentials.local.js');
 
-const token = 'YOUR_DISCORD_BOT_TOKEN_HERE';
+// Configure WIT-AI
+const witClient = new Wit({
+    accessToken: credentials.WIT_TOKEN,
+    logger: new log.Logger(log.DEBUG) // optional
+});
 
-bot.login(token);
+bot.login(credentials.BOT_TOKEN);
 bot.on('ready', () => {
     console.log('O Bot do Hadou está pronto');
 });
 
 bot.on('message', msg => {
-    if ( msg.content.includes('Salve') || msg.content.includes('Oi') ) {
-       msg.reply('Olá seja bem vindo ao Canal do Hadou!'); 
+    if ( msg.author.bot ) {
+      console.log('Is author the bot?')
+      return false;
     }
-})
+
+    if ( msg.content.toLowerCase().startsWith('bot') ) {
+        if (  msg.content.includes('me conhece')  || msg.content.includes('quem sou eu') ) {
+            msg.content = 'Quem é ' + msg.author.username;
+        }
+
+        console.log('MSG:' + msg.content );        
+
+        witClient.message(msg.content).then((witSuccess) => {
+
+            
+            // Better response
+            let better = { 
+                confidence: 0, 
+                value: 'Desculpe, não te entendi.' 
+            }
+
+            for ( key in witSuccess.entities) {
+                let entity = witSuccess.entities[key];
+                for ( k in entity ) {
+                    if ( (entity[k].confidence*1000) > (better.confidence*1000) ) {
+                        better = entity[k];
+                    }                
+                }
+            }
+
+            msg.reply( better.value );
+        });        
+    }
+
+    // Log to teach the robot
+    let filename = './chat.log';
+    let chatContent = fs.readFileSync(filename, 'utf8');
+    fs.writeFile(filename, chatContent + msg.content + '\r\n', function(err){
+        if ( err ) {
+            return console.log(err);
+        }
+        console.log('File saved!');
+    });
+});
+
+bot.on('debug', console.log);
